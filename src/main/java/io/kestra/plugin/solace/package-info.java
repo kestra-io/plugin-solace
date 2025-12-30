4220c23bf41d1aeb54727f5eb71dc91d3eee484f
ben8t <pimpaudben@gmail.com>
2025-12-30 15:42:19 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/solace/package-info.java
+++ src/main/java/io/kestra/plugin/solace/package-info.java
@@ -1,6 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using the Solace broker.",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    categories = PluginSubGroup.PluginCategory.MESSAGING,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA
+    }
 )
 package io.kestra.plugin.solace;
 
